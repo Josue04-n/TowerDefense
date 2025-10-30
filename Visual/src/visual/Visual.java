64b44148f82bd3de1406ac4be0f9fdd1721b6ae1
Josue04-n <llumitasigjosue04@gmail.com>
2025-10-30 00:24:51 -0500

    Linea a borrar

--- Visual/src/visual/Visual.java
+++ Visual/src/visual/Visual.java
@@ -31,7 +31,8 @@ public class Visual {
         System.out.println("nueva rama");
         System.out.println("borra");
         System.out.println("borra");
-        System.out.println("despues der reverse");
+        System.out.println("despues der reverse");        
+
     }
 
 }
