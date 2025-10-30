95089687d7b7f7851d5eb490caed369c556989cc
Josue04-n <llumitasigjosue04@gmail.com>
2025-10-29 23:25:30 -0500

    Hola a todos

---
+++ Holax2/src/holax2/Holax2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package holax2;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Holax2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola amigos");
+        System.out.println("Quiero pasar la materia");
+    }
+    
+}
