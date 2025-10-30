20382ec27476fb00d59756127abdc0f5f6a8c383
Josue04-n <llumitasigjosue04@gmail.com>
2025-10-29 23:41:39 -0500

    repasando visual

---
+++ Visual/src/visual/Visual.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package visual;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Visual {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("holii lopez");
+        System.out.println("holii vero");
+    }
+    
+}
